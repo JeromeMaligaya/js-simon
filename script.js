ef80097caaf96d5d05d8f2b7d28236c62db0f463
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-27 17:51:56 +0200

    added index and style

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// 1.recupero l'elemento dal Dom
+/*scaletta
+fare la parte di markup
+// 1.recupero l'elemento dal Dom (timer, 5 values, button play)
 
 //2.raccolta dati
-    
+preparo la funzione per i 5 numeri casuali diversi
+li stampo in pagina    
+
+implemento un timer di 30 secondi (decremento)
+    invoco la funz e stampo i numeri casuali
+    inizia il countdown di 30 sec
+    scaduto il timer i numeri spariscono
 // 3.lavorazione dati
+preparo evento al click
+    recupero le userValues in un array
+    assegno score e message
+    confronto gli elementi dei due array
+    score aumenta o meno
     
 // 4.generazione output
+stampo score e message
+*/
