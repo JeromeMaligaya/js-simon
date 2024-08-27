2411af123bb5bcd494ed9d02c078a8735a0e4c65
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-27 18:10:23 +0200

    recupero gli elementi dal dom

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-/*scaletta
-fare la parte di markup
-// 1.recupero l'elemento dal Dom (timer, 5 values, button play)
 
-//2.raccolta dati
-preparo la funzione per i 5 numeri casuali diversi
-li stampo in pagina    
+/// 1.recupero l'elemento dal Dom (timer, 5 values, button play)
+const timer = document.getElementById('timer');
+const buttonPlay = document.querySelector('button');
+const userNumber1 = document.querySelector('userNumber1').value;
+const userNumber2 = document.querySelector('userNumber2').value;
+const userNumber3 = document.querySelector('userNumber3').value;
+const userNumber4 = document.querySelector('userNumber4').value;
+const userNumber5 = document.querySelector('userNumber5').value;
 
-implemento un timer di 30 secondi (decremento)
-    invoco la funz e stampo i numeri casuali
-    inizia il countdown di 30 sec
-    scaduto il timer i numeri spariscono
+///2.raccolta dati
+// preparo la funzione per i 5 numeri casuali diversi
+// implemento un timer di 30 secondi (decremento)
+// invoco la funz e stampo i numeri casuali
+// inizia il countdown di 30 sec
+// scaduto il timer i numeri spariscono
 // 3.lavorazione dati
-preparo evento al click
-    recupero le userValues in un array
-    assegno score e message
-    confronto gli elementi dei due array
-    score aumenta o meno
+// preparo evento al click
+// recupero le userValues in un array
+const userNumbers = [userNumber1, userNumber2, userNumber3, userNumber4,userNumber5];
+// assegno score e message
+// confronto gli elementi dei due array
+// score aumenta o meno
     
-// 4.generazione output
-stampo score e message
-*/
+/// 4.generazione output
+// stampo score e message
+
