4603549dfdb16675f34cc70ed8b930d26ee83630
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-27 22:42:49 +0200

    added arrays comparison

--- script.js
+++ script.js
@@ -68,7 +68,18 @@ console.log('userNumbers' , userNumbers);
 let score = 0;
 let message = '';
 // confronto gli elementi dei due array
-
+for(let i = 0; i < randomNumbers.length; i++){
+    // verifico se gli elementi di randomNumbers[] sono contenuti in userNumbers[] 
+    if(userNumbers.includes(randomNumbers[i])){
+        // incremento score ogni volta che trovo un elemento in comune
+        score++;
+    } 
+    message = `Hai azzeccato ${score} parole. <span class="text-danger">Riprova!!</span>`;
+    if (score === 0) message= `<span class="text-danger">Hai totalizzato 0 punti!!!!</span>`;
+    if (score === 5) message = `<span class="text-success">HAI VINTOOOO!! Hai azzeccato tutti i ${score} numeri.</span>`;
+    console.log(score)
+}
+    
 // 4.generazione output
 // stampo score e message
 
